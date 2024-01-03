16a0c7072f128ebf7782602a090d14bb36a5cab0
Anthony <anthony.rousseau26@gmail.com>
2024-01-03 16:38:16 +0100

    Implementing Framer Motion

--- 30-react-anitmation/src/components/ChallengeItem.jsx
+++ 30-react-anitmation/src/components/ChallengeItem.jsx
@@ -1,4 +1,5 @@
 import { useContext } from 'react';
+import { motion } from 'framer-motion';
 
 import { ChallengesContext } from '../store/challenges-context.jsx';
 
@@ -31,7 +32,7 @@ export default function ChallengeItem({
       <article className="challenge-item">
         <header>
           <img {...challenge.image} />
-          <div className='challenge-item-meta'>
+          <div className="challenge-item-meta">
             <h2>{challenge.title}</h2>
             <p>Complete until {formattedDate}</p>
             <p className="challenge-item-actions">
@@ -42,11 +43,16 @@ export default function ChallengeItem({
             </p>
           </div>
         </header>
-        <div className={`challenge-item-details ${isExpanded && 'expanded'}`}>
+        <div className="challenge-item-details">
           <p>
             <button onClick={onViewDetails}>
               View Details{' '}
-              <span className="challenge-item-details-icon">&#9650;</span>
+              <motion.span
+                animate={{ rotate: isExpanded ? 180 : 0 }}
+                className="challenge-item-details-icon"
+              >
+                &#9650;
+              </motion.span>
             </button>
           </p>
 
