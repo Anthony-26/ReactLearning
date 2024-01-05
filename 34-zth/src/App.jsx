e981cd240863a341cd528c99b39e1253c9c516fe
Anthony <anthony.rousseau26@gmail.com>
2024-01-05 17:01:57 +0100

    Adding basic component

---
+++ 34-zth/src/App.jsx
@@ -0,0 +1,7 @@
+import Post from "./component/Post";
+
+function App() {
+  return <h1>Hello World!</h1>;
+}
+
+export default App;
---
+++ 34-zth/src/component/Post.jsx
@@ -0,0 +1,5 @@
+const names = [];
+
+export default function Post() {
+  return <></>;
+}
