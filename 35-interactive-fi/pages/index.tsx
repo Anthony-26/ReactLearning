7cff33e3a5c4985cf35a809ad25f872341d2329b
Anthony <anthony.rousseau26@gmail.com>
2024-01-12 13:45:43 +0100

    update

---
+++ 35-interactive-fi/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../components/Header/Header';
+
+const HomePage = () => {
+  return (
+    <div>
+      <Header />
+      <p>Hello</p>
+    </div>
+  );
+};
+
+export default HomePage;
